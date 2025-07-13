1e305677843883a0884f3c2ce2e249e24f9886a1
Trydimas <akentev.dima@bk.ru>
2025-07-14 01:10:36 +0300

    add health_check feature

--- router/health_check.py
+++ router/health_check.py
@@ -1,1 +1,9 @@
-#TODO make it
+from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
+
+router = APIRouter()
+
+
+@router.get("")
+async def health_check() -> PlainTextResponse:
+    return PlainTextResponse("OK!")
